121de4b458f9e9cd1cbaa00d336b86fd73f07b7c
Jonas Schubert <jonas.schubert@siemens.com>
2023-10-01 17:02:11 +0200

    fix(ms-teams-plugin): mark as module

---
+++ plugins/ms-teams/index.js
@@ -0,0 +1,5 @@
+import verifyConditions from './verifyConditions.js';
+import success from './success.js';
+import fail from './fail.js';
+
+export default { verifyConditions, success, fail };
--- plugins/ms-teams/index.mjs
+++
@@ -1,5 +0,0 @@
-import verifyConditions from './verifyConditions.mjs';
-import success from './success.mjs';
-import fail from './fail.mjs';
-
-export default { verifyConditions, success, fail };
